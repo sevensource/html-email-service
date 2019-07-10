8b193ca44b1f322996d3779b58a79b713c138dbc
pgaschuetz <philipp@sevensource.org>
2019-07-10 21:32:16 +0200

    * add PooledJavaMailSenderImpl

---
+++ src/main/java/org/sevensource/commons/email/configuration/PooledJavaMailSenderConfiguration.java
@@ -0,0 +1,20 @@
+package org.sevensource.commons.email.configuration;
+
+import javax.mail.Session;
+
+import org.sevensource.commons.email.javamail.PooledJavaMailSenderImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class PooledJavaMailSenderConfiguration extends AbstractJavaMailSenderConfiguration<PooledJavaMailSenderImpl>{
+
+	@Autowired(required = true)
+	PooledJavaMailSenderPoolConfiguration pooledJavaMailSenderPoolConfiguration;
+
+	@Override
+	protected PooledJavaMailSenderImpl getJavaMailSenderInstance(Session session) {
+		return new PooledJavaMailSenderImpl(session, pooledJavaMailSenderPoolConfiguration);
+	}
+}
---
+++ src/main/java/org/sevensource/commons/email/configuration/PooledJavaMailSenderPoolConfiguration.java
@@ -0,0 +1,7 @@
+package org.sevensource.commons.email.configuration;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
+public class PooledJavaMailSenderPoolConfiguration extends GenericObjectPoolConfig {
+
+}
